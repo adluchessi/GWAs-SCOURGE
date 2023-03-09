8807faf859c2c177f0bb2930ff8c66da7192252b
Andre Luchessi <64751224+adluchessi@users.noreply.github.com>
2023-03-09 17:10:57 -0500

    Update snptest script.sh

--- SNPTEST/snptest-array-example.sh
+++ SNPTEST/snptest-array-example.sh
@@ -1,22 +1,19 @@
 #!/bin/bash
-#SBATCH --partition=generalq
-#SBATCH --job-name=my-snptest-job-1
-#SBATCH --array=1-22
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=youremail@address.com
-#SBATCH --ntasks=3
+#SBATCH --job-name=post-snptest-sevcont
+#SBATCH --array=1-23
+#SBATCH --nodes=1 #número de nós
+#SBATCH --ntasks=23 #número total de tarefas
 #SBATCH --mem=5gb
-#SBATCH --time=12:0:0
-#SBATCH --output=/scratch/my-snptest-job-1.out
-#SBATCH --error=/scratch/my-snptest-job-1.err
+#SBATCH --time=0-6:00
+#SBATCH --mail-user=andre.luchessi@outlook.com
+#SBATCH --mail-type=ALL
+#SBATCH --output=/home/adluchessi/scratch/my-snptest-job-1.out
+#SBATCH --error=/home/adluchessi/scratch/my-snptest-job-1.err
 
-module load snptest/2.5.2
-
-snptest_v2.5.2 \
-        -data /scratch/chr${SLURM_ARRAY_TASK_ID}.vcf.gz /scratch/phenotype.sample \
-        -o /scratch/GWAS-myphenotype-chr${SLURM_ARRAY_TASK_ID}.out \
-        -genotype_field GP \
+srun /home/adluchessi/snptest_v2.5.6/snptest_v2.5.6  \
+        -data /home/adluchessi/ped_files/chr${SLURM_ARRAY_TASK_ID}.dose.maf01.R28.onlysnp.uniq3503.snptest.bed /home/adluchessi/pheno_files/pheno_oficial_severity_cont_snptest.txt \
+        -o results_files/severity_cont/severity_cont_${SLURM_ARRAY_TASK_ID}.out \
         -frequentist 1 \
         -method expected \
-        -pheno myphenotype \
-        -cov_names sex age pc1 pc2 pc3 pc4 pc5
+        -pheno severity \
+        -cov_names age sex agesquare agesex PC1 PC2 PC3 PC4 PC5 PC6 PC7 PC8 PC9 PC10
